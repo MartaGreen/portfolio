15a694a1324a28854c5c2875197c251e356364d9
Mariia Portnykh <portnykhmaria@gmail.com>
2021-06-27 17:37:25 +0400

    feat: add flip btn img

--- english-for-kids-base/src/scripts/cards.js
+++ english-for-kids-base/src/scripts/cards.js
@@ -24,6 +24,25 @@ function createAudio(src, classes) {
     classes.forEach((cls) => newAudio.classList.add(cls));
     return newAudio;
 }
+function createFlipBtn() {
+    const flipBtn = document.createElement("div");
+    flipBtn.setAttribute("class", "flipBtn");
+    flipBtn.innerHTML = `<?xml version="1.0" encoding="iso-8859-1"?>
+  <!-- Generator: Adobe Illustrator 16.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+  <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+  <svg class='flipBtnImg' version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+     width="25px" height="25px" viewBox="0 0 414.161 414.162" style="enable-background:new 0 0 414.161 414.162;"
+     xml:space="preserve">
+  <g>
+    <path d="M198.708,28.013c-133.206,0-175.424,126.655-175.424,126.655s152.13-117.915,256.945-37.848l-67.693,38.575l162.323,56.046
+      l39.302-163.05l-68.419,33.488C345.751,81.879,304.986,28.013,198.708,28.013z"/>
+    <path d="M133.932,297.341l67.693-38.575L39.302,202.72L0,365.77l68.419-33.487c0,0,40.765,53.865,147.033,53.865
+      c133.206,0,175.425-126.655,175.425-126.655S238.747,377.408,133.932,297.341z"/>
+  </g>
+  </svg>
+  `;
+    return flipBtn;
+}
 class Card {
     constructor(src, name) {
         this.stage = "";
@@ -53,17 +72,14 @@ class CategoryCard extends Card {
         const canvConts = this.cardContainer.querySelectorAll(".canvContainer");
         canvConts[0].classList.add("frontCard");
         canvConts[1].classList.add("backCard");
-        const flipBtn = document.createElement("button");
-        flipBtn.setAttribute("class", "flipBtn");
+        const flipBtn = createFlipBtn();
         canvConts[0].appendChild(flipBtn);
         return this.cardContainer;
     }
     playSound() {
         this.cardContainer.addEventListener("click", (event) => {
-            const flipBtn = this.cardContainer.querySelector(".flipBtn");
+            const flipBtn = this.cardContainer.querySelector(".flipBtnImg");
             if (event.target !== flipBtn) {
-                console.log(event.target);
-                console.log("play sound");
                 this.sound.play();
             }
         });
@@ -72,13 +88,11 @@ class CategoryCard extends Card {
         const flipBtn = this.cardContainer.querySelector(".flipBtn");
         const cardContainer = this.cardContainer;
         flipBtn.addEventListener("click", function clickEvent() {
-            console.log("klick");
             const frontCard = cardContainer.querySelector(".frontCard");
             const backCard = cardContainer.querySelector(".backCard");
             frontCard.classList.add("flipBack");
             backCard.classList.add("flipFront");
             cardContainer.addEventListener("mouseleave", () => {
-                console.log("mouse out");
                 frontCard.classList.remove("flipBack");
                 backCard.classList.remove("flipFront");
             });
--- english-for-kids-base/src/tsScripts/cards.ts
+++ english-for-kids-base/src/tsScripts/cards.ts
@@ -34,6 +34,26 @@ function createAudio(src: string, classes: string[]) {
   return newAudio;
 }
 
+function createFlipBtn() {
+  const flipBtn: HTMLDivElement = document.createElement("div");
+  flipBtn.setAttribute("class", "flipBtn");
+  flipBtn.innerHTML = `<?xml version="1.0" encoding="iso-8859-1"?>
+  <!-- Generator: Adobe Illustrator 16.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+  <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+  <svg class='flipBtnImg' version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+     width="25px" height="25px" viewBox="0 0 414.161 414.162" style="enable-background:new 0 0 414.161 414.162;"
+     xml:space="preserve">
+  <g>
+    <path d="M198.708,28.013c-133.206,0-175.424,126.655-175.424,126.655s152.13-117.915,256.945-37.848l-67.693,38.575l162.323,56.046
+      l39.302-163.05l-68.419,33.488C345.751,81.879,304.986,28.013,198.708,28.013z"/>
+    <path d="M133.932,297.341l67.693-38.575L39.302,202.72L0,365.77l68.419-33.487c0,0,40.765,53.865,147.033,53.865
+      c133.206,0,175.425-126.655,175.425-126.655S238.747,377.408,133.932,297.341z"/>
+  </g>
+  </svg>
+  `
+  return flipBtn
+}
+
 class Card {
   stage: string;
   src: string;
@@ -82,8 +102,7 @@ class CategoryCard extends Card {
     canvConts[0].classList.add("frontCard");
     canvConts[1].classList.add("backCard");
 
-    const flipBtn: HTMLButtonElement = document.createElement("button");
-    flipBtn.setAttribute("class", "flipBtn");
+    const flipBtn = createFlipBtn();
     canvConts[0].appendChild(flipBtn);
 
     return this.cardContainer;
@@ -91,10 +110,8 @@ class CategoryCard extends Card {
 
   playSound() {
     this.cardContainer.addEventListener("click", (event) => {
-      const flipBtn = this.cardContainer.querySelector(".flipBtn");
+      const flipBtn:HTMLElement = this.cardContainer.querySelector(".flipBtnImg");
       if (event.target !== flipBtn) {
-        console.log(event.target);
-        console.log("play sound");
         this.sound.play();
       }
     });
@@ -105,7 +122,6 @@ class CategoryCard extends Card {
       this.cardContainer.querySelector(".flipBtn");
     const cardContainer: HTMLDivElement = this.cardContainer;
     flipBtn.addEventListener("click", function clickEvent() {
-      console.log("klick");
       const frontCard: HTMLButtonElement =
         cardContainer.querySelector(".frontCard");
       const backCard: HTMLButtonElement =
@@ -115,7 +131,6 @@ class CategoryCard extends Card {
       backCard.classList.add("flipFront");
 
       cardContainer.addEventListener("mouseleave", () => {
-        console.log("mouse out")
         frontCard.classList.remove("flipBack");
         backCard.classList.remove("flipFront");
       });
